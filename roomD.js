9e402be0993ec47f8521de580f267e7ad75a2c59
NatnichaTongrod <49260459+NatnichaTongrod@users.noreply.github.com>
2019-04-28 14:03:59 +0700

    Add files via upload

---
+++ roomD.js
@@ -0,0 +1,48 @@
+var room = new Vue({
+    el:"#room-detail",
+    data:{
+            name:"Room Detail"
+
+    }
+})
+
+var  standard = new Vue({
+    el:"#standard-room",
+    data:{
+        name:"Standard Room",
+        no:5,
+        facilities:"Room Facilities",
+        des:"•Individual Controlled Air Conditioning\n•32'' LED TV with Satellite Programs\n•In room Safe\n•Minibar\n•Telephone (International Direct Dial)\n•Complimentary Wi-Fi Internet Access\n•Hair Dryer\n•Umbrella\n*FOR 2 PERSONS*",
+        
+    }
+})
+
+var superior = new Vue({
+    el:"#superior-room",
+    data:{
+        name:"Superior Room",
+        no:3,
+        facilities:"Room Facilities",
+        des:"•Individual Controlled Air Conditioning\n•32'' LED TV with Satellite Programs\n•Electronic Door Locking System\n•In room Safe\n•Minibar\n•Telephone (International Direct Dial)\n•Complimentary Wi-Fi Internet Access\n•Hair Dryer\n•Umbrella\n*FOR 2 PERSONS*"
+    }
+})
+
+var deluxe = new Vue({
+    el:"#deluxe-room",
+    data:{
+        name:"Deluxe Room",
+        no:1,
+        facilities:"Room Facilities",
+        des:"•Individual Controlled Air Conditioning\n•32'' LED TV with Satellite Program\n•Electronic Door Locking System\n•In room Safe\n•Minibar\n•Tea and Coffee facilities\n•Complimentary Wi-Fi Internet Access\n•Hair Dryer\n•Bathrobes\n•Umbrella\n*1 BEDROOM / 1 BATHROOM / 1 LIVING ROOM - 2 PERSONS*"
+    }
+})
+
+var suite = new Vue({
+    el:"#suite-room",
+    data:{
+        name:"Suite Room",
+        no:2,
+        facilities:"Room Facilities",
+        des:"•Individual Controlled Air Conditioning\n•32'' LED TV with Satellite Program\n•In room Safe\n•Minibar\n•Tea and Coffee facilities\n•Complimentary Wi-Fi Internet Access\n•Hair Dryer\n•Bathrobes\n•Umbrella\n*2 BEDROOM / 2 BATHROOM / 1 LIVING ROOM - 4 PERSONS*"
+    }
+})
